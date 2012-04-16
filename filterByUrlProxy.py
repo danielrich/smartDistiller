5d33bf96df18e3adb6c8e2aef9cccc451b2438e9
Daniel Rich <qianyilong@gmail.com>
2012-04-16 15:32:51 -0500

    I need to pull this file local

--- filterByUrlProxy.py
+++ filterByUrlProxy.py
@@ -11,43 +11,7 @@ from twisted.web.http import HTTPClient, Request, HTTPChannel
 
 import sys
 
-class modifiedProxyRequest(Request):
-   """
-   Used by Proxy to implement a simple web proxy.
-   Code pulled from twisted. MIT license just a temp test
-   @ivar reactor: the reactor used to create connections.
-   @type reactor: object providing L{twisted.internet.interfaces.IReactorTCP}
-   """
-
-   protocols = {'http': proxy.ProxyClientFactory}
-   ports = {'http': 80}
-
-   def __init__(self, channel, queued, reactor=reactor):
-      Request.__init__(self, channel, queued)
-      self.reactor = reactor
-
-
-   def process(self):
-      print ("WHO")
-      parsed = urlparse.urlparse(self.uri)
-      protocol = parsed[0]
-      host = parsed[1]
-      port = self.ports[protocol]
-      if ':' in host:
-         host, port = host.split(':')
-         port = int(port)
-      rest = urlparse.urlunparse(('', '') + parsed[2:])
-      if not rest:
-         rest = rest + '/'
-      class_ = self.protocols[protocol]
-      headers = self.getAllHeaders().copy()
-      if 'host' not in headers:
-         headers['host'] = host
-      self.content.seek(0, 0)
-      s = self.content.read()
-      clientFactory = class_(self.method, rest, self.clientproto, headers,
-      s, self)
-      self.reactor.connectTCP(host, port, clientFactory)
+log.startLogging(sys.stdout)
 
 # I am stamping on the existing proxy
 proxy.ProxyRequest = modifiedProxyRequest
