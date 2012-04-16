11ca75289f776568dc6324428901d5988e702bd3
Daniel Rich <qianyilong@gmail.com>
2012-04-16 15:55:42 -0500

    basic filter based on boolean yes and no

--- filterByUrlProxy.py
+++ filterByUrlProxy.py
@@ -1,20 +1,12 @@
-from twisted.web import proxy, http
+import proxy
+from twisted.web import http
 from twisted.internet import reactor
 from twisted.python import log
 import urlparse
 from urllib import quote as urlquote
-
-from twisted.internet.protocol import ClientFactory
-from twisted.web.resource import Resource
-from twisted.web.server import NOT_DONE_YET
-from twisted.web.http import HTTPClient, Request, HTTPChannel
-
 import sys
 
-log.startLogging(sys.stdout)
-
-# I am stamping on the existing proxy
-proxy.ProxyRequest = modifiedProxyRequest
+#log.startLogging(sys.stdout)
 
 class ProxyFactory(http.HTTPFactory):
    protocol = proxy.Proxy
--- proxy.py
+++ proxy.py
@@ -139,8 +139,14 @@ class ProxyRequest(Request):
         Request.__init__(self, channel, queued)
         self.reactor = reactor
 
+    def failFiltering(self):
+        return False
+
 
     def process(self):
+        if self.failFiltering() :
+           self.uri = "http://localhost"
+
         parsed = urlparse.urlparse(self.uri)
         protocol = parsed[0]
         host = parsed[1]
@@ -161,6 +167,16 @@ class ProxyRequest(Request):
                                s, self)
         self.reactor.connectTCP(host, port, clientFactory)
 
+class ProxyFilteredRequest(ProxyRequest):
+    """
+    Used by Proxy to implement a simple web proxy.
+
+    @ivar reactor: the reactor used to create connections.
+    @type reactor: object providing L{twisted.internet.interfaces.IReactorTCP}
+    """
+    def failFiltering(self):
+        print ("whow filtering")
+        return True
 
 
 class Proxy(HTTPChannel):
@@ -178,126 +194,4 @@ class Proxy(HTTPChannel):
     a fully-functioning web proxy!
     """
 
-    requestFactory = ProxyRequest
-
-
-
-class ReverseProxyRequest(Request):
-    """
-    Used by ReverseProxy to implement a simple reverse proxy.
-
-    @ivar proxyClientFactoryClass: a proxy client factory class, used to create
-        new connections.
-    @type proxyClientFactoryClass: L{ClientFactory}
-
-    @ivar reactor: the reactor used to create connections.
-    @type reactor: object providing L{twisted.internet.interfaces.IReactorTCP}
-    """
-
-    proxyClientFactoryClass = ProxyClientFactory
-
-    def __init__(self, channel, queued, reactor=reactor):
-        Request.__init__(self, channel, queued)
-        self.reactor = reactor
-
-
-    def process(self):
-        """
-        Handle this request by connecting to the proxied server and forwarding
-        it there, then forwarding the response back as the response to this
-        request.
-        """
-        self.received_headers['host'] = self.factory.host
-        clientFactory = self.proxyClientFactoryClass(
-            self.method, self.uri, self.clientproto, self.getAllHeaders(),
-            self.content.read(), self)
-        self.reactor.connectTCP(self.factory.host, self.factory.port,
-                                clientFactory)
-
-
-
-class ReverseProxy(HTTPChannel):
-    """
-    Implements a simple reverse proxy.
-
-    For details of usage, see the file examples/reverse-proxy.py.
-    """
-
-    requestFactory = ReverseProxyRequest
-
-
-
-class ReverseProxyResource(Resource):
-    """
-    Resource that renders the results gotten from another server
-
-    Put this resource in the tree to cause everything below it to be relayed
-    to a different server.
-
-    @ivar proxyClientFactoryClass: a proxy client factory class, used to create
-        new connections.
-    @type proxyClientFactoryClass: L{ClientFactory}
-
-    @ivar reactor: the reactor used to create connections.
-    @type reactor: object providing L{twisted.internet.interfaces.IReactorTCP}
-    """
-
-    proxyClientFactoryClass = ProxyClientFactory
-
-
-    def __init__(self, host, port, path, reactor=reactor):
-        """
-        @param host: the host of the web server to proxy.
-        @type host: C{str}
-
-        @param port: the port of the web server to proxy.
-        @type port: C{port}
-
-        @param path: the base path to fetch data from. Note that you shouldn't
-            put any trailing slashes in it, it will be added automatically in
-            request. For example, if you put B{/foo}, a request on B{/bar} will
-            be proxied to B{/foo/bar}.  Any required encoding of special
-            characters (such as " " or "/") should have been done already.
-
-        @type path: C{str}
-        """
-        Resource.__init__(self)
-        self.host = host
-        self.port = port
-        self.path = path
-        self.reactor = reactor
-
-
-    def getChild(self, path, request):
-        """
-        Create and return a proxy resource with the same proxy configuration
-        as this one, except that its path also contains the segment given by
-        C{path} at the end.
-        """
-        return ReverseProxyResource(
-            self.host, self.port, self.path + '/' + urlquote(path, safe=""),
-            self.reactor)
-
-
-    def render(self, request):
-        """
-        Render a request by forwarding it to the proxied server.
-        """
-        # RFC 2616 tells us that we can omit the port if it's the default port,
-        # but we have to provide it otherwise
-        if self.port == 80:
-            host = self.host
-        else:
-            host = "%s:%d" % (self.host, self.port)
-        request.received_headers['host'] = host
-        request.content.seek(0, 0)
-        qs = urlparse.urlparse(request.uri)[4]
-        if qs:
-            rest = self.path + '?' + qs
-        else:
-            rest = self.path
-        clientFactory = self.proxyClientFactoryClass(
-            request.method, rest, request.clientproto,
-            request.getAllHeaders(), request.content.read(), request)
-        self.reactor.connectTCP(self.host, self.port, clientFactory)
-        return NOT_DONE_YET
+    requestFactory = ProxyFilteredRequest
